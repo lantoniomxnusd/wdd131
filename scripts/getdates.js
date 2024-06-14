5dd67452da1ac49324c00dd41b5f35bb1b525761
lantoniomxnusd <luisoteacher@gmail.com>
2024-06-13 22:10:05 -0600

    bugs

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,3 +5,5 @@ year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 const lastModifiedText = document.querySelector("#lastModified");
 const lastModified = document.lastModified;
 lastModifiedText.textContent = `Last Modified: ${lastModified}`;
+
+// test
