af0b4a0c466a5c42f3d244dfb6675a44de96adc7
lantoniomxnusd <luisoteacher@gmail.com>
2024-06-13 21:17:24 -0600

    s

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,7 @@
 const year = document.querySelector("#currentyear");
-
 const today = new Date();
-
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
-const lastModified = document.lastModified;
 const lastModifiedText = document.querySelector("#lastModified");
-
-
+const lastModified = document.lastModified;
 lastModifiedText.textContent = `Last Modified: ${lastModified}`;
