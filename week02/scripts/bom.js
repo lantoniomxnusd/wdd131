9b547cc8d23d2c532892c1984e06b02b27836129
lantoniomxnusd <luisoteacher@gmail.com>
2025-05-17 22:53:11 -0600

    added  chapter and delete button

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,15 +1,30 @@
-const input = document.querySelector('#favchap');
+const input = document.getElementById('favchap');
 const button = document.querySelector('button');
-const list = document.querySelector('#list');
+const list = document.getElementById('list');
 
-const li = document.createElement('li');
-const deleteButton = document.createElement('button');
 
-li.textContent = input.value;
 
-deleteButton.textContent ='❌';
 
-li.append(deleteButton);
-list.append(li);
 
+
+
+button.addEventListener('click', ()=> {
+   if (input.value.trim() !== ''){
+        const li = document.createElement('li');
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent ='❌';
+        li.append(deleteButton);
+        list.append(li);
+
+        deleteButton.addEventListener('click', () =>{
+        list.removeChild(li);
+        input.focus();
+         });
+
+        input.value ='';
+    }
+    input.focus();
+});
 
