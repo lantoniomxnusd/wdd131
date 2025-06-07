4f9c44313cdf695a88245e05a0f9ae2e8892896a
lantoniomxnusd <luisoteacher@gmail.com>
2025-06-06 21:42:03 -0600

    testing

--- scripts/form.js
+++ scripts/form.js
@@ -35,17 +35,19 @@ document.addEventListener("DOMContentLoaded", () => {
         option.textContent = product.name;
         select.appendChild(option);
     });
+
+    const visitsDisplay = document.querySelector(".visits");
+
+    let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+    if (numVisits !== 0) {
+        visitsDisplay.textContent = numVisits;
+    } else {
+        visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+    }
+    numVisits++;
+    localStorage.setItem("numVisits-ls", numVisits);
+
 });
 
 
 
-const visitsDisplay = document.querySelector(".visits");
-
-let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
-} else {
-	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
-}
-numVisits++;
-localStorage.setItem("numVisits-ls", numVisits);
