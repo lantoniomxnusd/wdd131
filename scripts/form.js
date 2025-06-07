ea2bf2e2bf4fef7023a69f35c9e526f5954ad0b3
lantoniomxnusd <luisoteacher@gmail.com>
2025-06-06 21:33:40 -0600

    should be finished

---
+++ scripts/form.js
@@ -0,0 +1,51 @@
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+document.addEventListener("DOMContentLoaded", () => {
+    const select = document.getElementById("productName");
+
+    products.forEach(product => {
+        const option = document.createElement("option");
+        option.value = product.id;
+        option.textContent = product.name;
+        select.appendChild(option);
+    });
+});
+
+
+
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,4 @@
-const year = document.querySelector("#currentyear");
+const year = document.getElementById("currentYear");
 const today = new Date();
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
